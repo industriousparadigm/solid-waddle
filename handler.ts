f96d1e5727e045fc0f82a04fc531fcd0382fcdd6
industriousparadigm <dsgmcosta@gmail.com>
2020-04-09 15:58:33 +0100

    trying some alexandria stuff and amp cors

--- handler.ts
+++ handler.ts
@@ -1,17 +1,54 @@
 import { Handler } from 'aws-lambda'
+import fetch from 'node-fetch'
 import 'source-map-support/register'
+import { getCorsOrigin, generateToken } from './helpers'
 
-export const quickSignup: Handler = async (event, _context, callback) => {
+const alexandriaEndpoint =
+  'https://staging.telemetry.report/api/v2/subscribers/quick-sign-up'
+
+const mockPayload = {
+  verticalID: 38,
+  email: 'michaeljordan@gmail.com',
+  firstname: 'Michael',
+  referrer: {
+    medium: 'medium-name',
+    pageURL: 'http://page-url.com',
+    campaign: 'campaign-name',
+    source: 'source-name',
+    referrerURL: 'http://google.com',
+    internalCTA: 'footer',
+    postID: 0,
+  },
+}
+
+const token = generateToken(mockPayload)
+
+const mockBody = { ...mockPayload, token }
+
+export const quickSignup: Handler = async (request, _context, callback) => {
+  // talk to alexandria
+  const alexandriaResponse = await fetch(alexandriaEndpoint, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: mockBody,
+  }).then((res) => res.json())
+
+  // send a response
   const response = {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Origin': getCorsOrigin(request),
+    },
     body: JSON.stringify(
       {
         message: "Go Serverless Webpack (TS)! 'quickSignup' endpoint hit!",
-        input: event,
+        request,
       },
       null,
       2
     ),
+    alexandriaResponse: JSON.stringify({ ...alexandriaResponse }, null, 2),
   }
 
   callback(null, response)
---
+++ helpers.ts
@@ -0,0 +1,50 @@
+import SHA1 from 'crypto-js/sha1'
+import encodeUtf8 from 'crypto-js/enc-utf8'
+import encodeBase64 from 'crypto-js/enc-base64'
+
+const generateToken = (payload) => {
+  const identifier = 'TelemetryPayload'
+
+  let stringOfPayloadValues = ''
+
+  for (let key in payload) {
+    if (typeof payload[key] === 'object') {
+      stringOfPayloadValues += Object.values(payload[key]).join('')
+    } else {
+      stringOfPayloadValues += payload[key]
+    }
+  }
+  // Create hash from values and identifier
+  const hash = SHA1(`${stringOfPayloadValues}${identifier}`)
+  // Convert string to word array
+  const wordArray = encodeUtf8.parse(`${hash}|${new Date().getTime()}`)
+  // Generate Base64 token from word array
+  return encodeBase64.stringify(wordArray)
+}
+
+const getCorsOrigin = (req) => {
+  /* this function should get the origin and put it into
+   * the Access-Control-Allow-Origin header if it exists
+   * in the list of allowed origins.
+   **/
+
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.stylist.co.uk',
+    'https://staging.stylist.co.uk',
+    'https://stylist-preprod.herokuapp.com',
+    'https://www.stylist.co.uk',
+    'https://www-stylist-co-uk.cdn.ampproject.org',
+    'https://cdn.ampproject.org',
+    'https://bing-amp.com',
+  ]
+
+  const publisherOrigin = 'https://www.stylist.co.uk'
+  const sourceOrigin = req?.headers?.origin
+
+  return allowedOrigins.indexOf(sourceOrigin) !== -1
+    ? sourceOrigin
+    : publisherOrigin
+}
+
+export { getCorsOrigin, generateToken }
